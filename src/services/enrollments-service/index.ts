1c6322a673d52078ff269f9ea60533d4e5720728
guilhermeicarofr <guilhermeicarofr@gmail.com>
2022-11-14 16:17:13 -0300

    feat: return address by cep implemented on route /enrollments/cep?cep=xxxxxxxx

--- src/services/enrollments-service/index.ts
+++ src/services/enrollments-service/index.ts
@@ -1,16 +1,30 @@
 import { request } from "@/utils/request";
-import { notFoundError, requestError } from "@/errors";
+import { notFoundError } from "@/errors";
 import addressRepository, { CreateAddressParams } from "@/repositories/address-repository";
 import enrollmentRepository, { CreateEnrollmentParams } from "@/repositories/enrollment-repository";
 import { exclude } from "@/utils/prisma-utils";
 import { Address, Enrollment } from "@prisma/client";
+import { CepAddress, ViaCEPAddress } from "@/protocols";
+import { AxiosResponse } from "axios";
 
-async function getAddressFromCEP() {
-  const result = await request.get("https://viacep.com.br/ws/37440000/json/");
+async function getAddressFromCEP(cep: string) {
+  const result = await request.get(`https://viacep.com.br/ws/${cep}/json/`) as AxiosResponse<ViaCEPAddress>;
+
+  // | AxiosResponse<{erro:boolean}>
 
   if (!result.data) {
     throw notFoundError();
   }
+
+  const address: CepAddress = {
+    logradouro: result.data.logradouro,
+    complemento: result.data.complemento,
+    bairro: result.data.bairro,
+    cidade: result.data.localidade,
+    uf: result.data.uf
+  };
+
+  return address;
 }
 
 async function getOneWithAddressByUserId(userId: number): Promise<GetOneWithAddressByUserIdResult> {
